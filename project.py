e466e3e063bcc9dbbf4eb3864b98355d1262db9d
DylGun <78462130+DylGun@users.noreply.github.com>
2022-09-21 19:19:57 -0700

    Add files via upload

---
+++ project.py
@@ -0,0 +1,16 @@
+from unicodedata import name
+
+
+def task():
+    user=input("y/n")
+def learn(task1):
+    if task1=="y":
+        print("Good job")
+
+    else: 
+        print("Complete the task")
+
+print("Learn how to code")
+print("Learn the syntax")
+name=task()
+learn(name)
